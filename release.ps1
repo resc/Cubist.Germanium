62133c39f1c9a4a4e2b17bdfba700833b5dca50d
resc <remco.schoeman@gmail.com>
2023-06-03 01:34:00 +0200

    --all doesn't push everything...

--- release.ps1
+++ release.ps1
@@ -12,7 +12,6 @@ if($LASTEXITCODE -ne  0) {
     exit 1 
 }
 
-
 # check if unit tests pass
 dotnet test ./src/src.sln
 if ($LASTEXITCODE -ne 0) { 
@@ -32,6 +31,8 @@ $releaseVersion = "v$releaseVersion"
 
 git tag $releaseVersion
 
-git push --all
+# push the current branch and the tag
+git push origin HEAD
+git push origin $releaseVersion
 
 gh release create $releaseVersion --verify-tag --generate-notes
